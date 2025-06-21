6bcc59ccc172cbab8432a10774f7e7c138c4db44
Silverio Rodriguez Alcorta <silverio.rodriguez@kuarxis.com>
2025-06-21 19:34:52 -0400

    Intermediate commit to backup local changes

--- amplify/backend/function/oncrebedevFunction20250621/src/app.js
+++ amplify/backend/function/oncrebedevFunction20250621/src/app.js
@@ -40,6 +40,20 @@ const { Parameters } = await (new aws.SSM())
 
 Parameters will be of the form { Name: 'secretName', Value: 'secretValue', ... }[]
 */
+/*
+Use the following code to retrieve configured secrets from SSM:
+
+const aws = require('aws-sdk');
+
+const { Parameters } = await (new aws.SSM())
+  .getParameters({
+    Names: ["MONGO_URI","JWT_SECRET","PAYPAL_CLIENT_ID","KUARSIS_AWS_PRODUCTS_S3_ACCESS_KEY","KUARSIS_AWS_PRODUCTS_S3_SECRET_KEY","KUARSIS_SURVEY_MONKEY_TOKEN","KUARSIS_SURVEY_MONKEY_WEBHOOKS_TOKEN","KUARSIS_SURVEY_MONKEY_APIKEY"].map(secretName => process.env[secretName]),
+    WithDecryption: true,
+  })
+  .promise();
+
+Parameters will be of the form { Name: 'secretName', Value: 'secretValue', ... }[]
+*/
 /**
  * Use the following code to retrieve configured secrets from SSM:
  *
@@ -348,6 +362,11 @@ app.use(function (req, res, next) {
  * Example get method *
  **********************/
 app.use('/users', userRoutes)
+
+// app.post('/users', function(req, res) {
+//   // Add your code here
+//   res.json({success: 'NEW Users post call succeed!', url: req.url, body: req.body})
+// });
 app.use('/products', productRoutes)
 app.use('/surveys', surveyRoutes)
 app.use('/orders', orderRoutes)
